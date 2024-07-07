7fe6ef48471e8c46f3ac958be3d8c691db6dc431
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2024-07-06 21:56:41 -0300

    finished project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("Ola Mundo");
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+import './style.css';
+
+const passwordElement = document.querySelector('#generateRadomPassawordButton');
+const displayPasswordElement = document.querySelector('h2');
+const copyButton = document.querySelector('#copyButton');
+
+passwordElement.addEventListener('click', () => {
+  const radomPassword = nanoid();
+  displayPasswordElement.innerHTML = radomPassword;
+});
+
+copyButton.addEventListener('click', () => {
+  const text = displayPasswordElement.textContent;
+  copy(text);
+});
